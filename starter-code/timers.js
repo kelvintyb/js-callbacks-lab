48dd398491daf012ac6faedcb680ca3ee02b2876
Kelvin Tan <kelvin.tyb@gmail.com>
2016-09-28 15:42:59 +0800

    stable version without bonus JS structure

--- starter-code/timers.js
+++ starter-code/timers.js
@@ -1,1 +1,30 @@
-// your code here:
+$('#reset').on('click', clickHandlerReset)
+$('#start').on('click', clickHandlerStart)
+$('#pause').on('click', clickHandlerPause)
+
+var timer = {
+  var seconds = 0,
+    timerId;
+
+  function updateTime() {
+    seconds += 1;
+    console.log(seconds);
+    $('#timer').text(seconds);
+  }
+
+  function clickHandlerStart() {
+    timerId = window.setInterval(updateTime, 1000);
+  }
+
+  // Stop -- but do not reset! -- the timer using clearInterval().
+  function clickHandlerPause() {
+    window.clearInterval(timerId);
+  }
+
+  function clickHandlerReset() {
+    window.clearInterval(timerId);
+    seconds = 0;
+    $('#timer').text('Stop Watch');
+  }
+
+}
